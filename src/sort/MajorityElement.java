8d053135bb8c415ec1870a470ba3887396ec5b2b
praveen dewangan <pdewangan@mersat-tech.com>
2018-07-14 01:35:12 +0530

    solved problem for majority Element

---
+++ src/sort/MajorityElement.java
@@ -0,0 +1,97 @@
+package sort;
+
+import java.util.*;
+import java.io.*;
+
+public class MajorityElement {
+    private  static  boolean isMajority = false;
+    private static int getMajorityElement(int[] arr, int left, int right, int totalSize) {
+        if (left > right) return 0;
+
+        int lb = left;
+        int ub = right;
+        int i = left;
+        int pivot = arr[left];
+        int count = 0;
+
+        while (i <= ub) {
+            int diff = pivot - arr[i];
+            if (diff > 0) {
+                int temp = arr[i];
+                arr[i] = arr[lb];
+                arr[lb] = temp;
+
+                i++;
+                lb++;
+            } else if (diff == 0) {
+                i++;
+                count++;
+            } else {
+                int temp = arr[i];
+                arr[i] = arr[ub];
+                arr[ub] = temp;
+
+                ub--;
+            }
+        }
+
+        if ((lb-1-left) > (totalSize/2)) {
+            getMajorityElement(arr, left, lb -1, totalSize);
+        }
+
+        if ( (right - ub + 1) > (totalSize/2)) {
+            getMajorityElement(arr, ub + 1, right, totalSize);
+        }
+
+        if (count > (totalSize /2)) {
+            isMajority = true;
+            return 1;
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        getMajorityElement(a, 0, a.length -1, a.length);
+        if (isMajority) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+}
--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -16,8 +16,9 @@ public class QuickSort {
         basicQuickSort(arr, 0, arr.length - 1);
     }
 
+    // will also handle and duplicate element
     public void advanceSort(int[] arr) {
-
+        advanceQuickSort(arr, 0, arr.length-1);
     }
 
     private void basicQuickSort(int[] arr, int start, int end) {
@@ -49,15 +50,58 @@ public class QuickSort {
         int temp = arr[front];
         arr[front] = arr[start];
         arr[start] = temp;
-        return  front;
+        return front;
+    }
+
+    private void advanceQuickSort(int[] arr, int start, int end) {
+        if (start >= end) return;
+
+        Integer lb = start;
+        Integer ub = end;
+
+        int[] props = advanceQuickPartition(arr, start, end, lb, ub);
+
+        advanceQuickSort(arr,start, props[0]-1);
+        advanceQuickSort(arr,props[1]+1, end);
+    }
+
+    private int[] advanceQuickPartition(int[] arr, int start, int end, Integer lb, Integer ub) {
+
+        int pivotValue = arr[start];
+        int i = start;
+        while (i <= ub) {
+            int diff = pivotValue - arr[i];
+            // case1: if arr[i] < pivot :
+            // will exchange arr[i] and arr[lb] and increment lb and i as well so all the small element
+            // will come left to pivot
+            if (diff > 0) {
+                int temp = arr[i];
+                arr[i] = arr[lb];
+                arr[lb] = temp;
+                i++;
+                lb++;
+            } else if (diff == 0) {
+                // will just increment i
+                i++;
+            } else {
+                // will exchange element from ub to i'th position
+                // so that on Right side only grater then pivot element will be there
+                // and we decrement the ub as well
+                int temp = arr[ub];
+                arr[ub] = arr[i];
+                arr[i] = temp;
+                ub--;
+            }
+        }
+        return new int[]{lb, ub, i };
     }
 
     public static void main(String[] args) {
-        int[] arr = {4,7,4,1,9,5,7,3,2,5};
+        int[] arr = {4, 7, 4, 1, 9, 4, 5, 7, 3, 2, 4,5};
         QuickSort quickSort = new QuickSort(arr);
-        quickSort.basicSort(arr);
+        quickSort.advanceSort(arr);
 
-        Arrays.stream(arr).forEach(data -> System.out.println(" "+data));
+        Arrays.stream(arr).forEach(data -> System.out.print(" " + data));
     }
 
 }
